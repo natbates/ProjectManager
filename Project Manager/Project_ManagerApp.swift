f6c3e6cc53757f89b1444c703dc09ede0244df0e
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2024-03-12 23:16:23 +0000

    Initial Commit

---
+++ Project Manager/Project_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_ManagerApp.swift
+//  Project Manager
+//
+//  Created by Nathaniel Bates on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
